83e7cb234e221473b2a7044837ef351210566080
John <jtseaman85@gmail.com>
2011-04-11 23:32:20 -0500

    added necessary implementations of function references in order for the shell to compile
    Signed-off-by: John <jtseaman85@gmail.com>

--- lab8/ush.c
+++ lab8/ush.c
@@ -4,13 +4,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#define FFLAG (O_WRONLY | O_CREAT | O_TRUNC)
+#define FMODE (S_IRUSR | S_IWUSR)
 #define BACK_SYMBOL '&'
-#define PROMPT_STRING "ush7>>"
+#define PROMPT_STRING "ush>>"
 #define QUIT_STRING "q"
 
 void executecmd(char *incmd);
+int makeargv(const char *s, const char *delimiters, char ***argvp);
+int parseandredirectin(char *s);
+int parseandredirectout(char *s);
 int signalsetup(struct sigaction *def, sigset_t *mask, void (*handler)(int));
 static sigjmp_buf jumptoprompt;
 static volatile sig_atomic_t okaytojump = 0;
@@ -80,6 +88,105 @@ int main (void) {
       while (waitpid(-1, NULL, WNOHANG) > 0);    /* wait for background procs */
    }
    return 0;
+}
+
+void executecmd(char *incmd) {
+   char **chargv;
+   if (parseandredirectout(incmd) == -1)
+      perror("Failed to redirect output");
+   else if (parseandredirectin(incmd) == -1)
+      perror("Failed to redirect input");
+   else if (makeargv(incmd, " \t", &chargv) <= 0)
+      fprintf(stderr, "Failed to parse command line\n");
+   else {
+      execvp(chargv[0], chargv);
+      perror("Failed to execute command");
+   }
+   exit(1);
+}
+
+int makeargv(const char *s, const char *delimiters, char ***argvp) {
+   int error;
+   int i;
+   int numtokens;
+   const char *snew;
+   char *t;
+
+   if ((s == NULL) || (delimiters == NULL) || (argvp == NULL)) {
+      errno = EINVAL;
+      return -1;
+   }
+   *argvp = NULL;                           
+   snew = s + strspn(s, delimiters);         /* snew is real start of string */
+   if ((t = malloc(strlen(snew) + 1)) == NULL) 
+      return -1; 
+   strcpy(t, snew);               
+   numtokens = 0;
+   if (strtok(t, delimiters) != NULL)     /* count the number of tokens in s */
+      for (numtokens = 1; strtok(NULL, delimiters) != NULL; numtokens++) ; 
+
+                             /* create argument array for ptrs to the tokens */
+   if ((*argvp = malloc((numtokens + 1)*sizeof(char *))) == NULL) {
+      error = errno;
+      free(t);
+      errno = error;
+      return -1; 
+   } 
+                        /* insert pointers to tokens into the argument array */
+   if (numtokens == 0) 
+      free(t);
+   else {
+      strcpy(t, snew);
+      **argvp = strtok(t, delimiters);
+      for (i = 1; i < numtokens; i++)
+          *((*argvp) + i) = strtok(NULL, delimiters);
+    } 
+    *((*argvp) + numtokens) = NULL;             /* put in final NULL pointer */
+    return numtokens;
+}
+
+int parseandredirectin(char *cmd) {    /* redirect standard input if '<' */
+   int error;
+   int infd;
+   char *infile;
+
+   if ((infile = strchr(cmd, '<')) == NULL)
+      return 0;
+   *infile = 0;                  /* take everything after '<' out of cmd */
+   infile = strtok(infile + 1, " \t");
+   if (infile == NULL)
+      return 0;
+   if ((infd = open(infile, O_RDONLY)) == -1)
+      return -1;
+   if (dup2(infd, STDIN_FILENO) == -1) {
+      error = errno;                       /* make sure errno is correct */
+      close(infd);
+      errno = error;
+      return -1;
+   }
+   return close(infd);
+}
+
+int parseandredirectout(char *cmd) {  /* redirect standard output if '>' */
+   int error;
+   int outfd;
+   char *outfile;
+
+   if ((outfile = strchr(cmd, '>')) == NULL)
+      return 0;
+   *outfile = 0;                  /* take everything after '>' out of cmd */
+   outfile = strtok(outfile + 1, " \t");  
+   if (outfile == NULL)
+      return 0;
+   if ((outfd = open(outfile, FFLAG, FMODE)) == -1)
+      return -1;
+   if (dup2(outfd, STDOUT_FILENO) == -1) {
+      error = errno;                        /* make sure errno is correct */
+      close(outfd);
+      errno = error;
+      return -1;
+   }
+   return close(outfd);
 }
 
 int signalsetup(struct sigaction *def, sigset_t *mask, void (*handler)(int)) {
